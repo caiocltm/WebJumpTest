f17230add969c35d0210544f9b820c5d8e5e22c8
caio.menezes <caio.menezes@github.com>
2020-02-29 01:00:47 -0300

    [WebJumpTest] Definição de atributo para o MongoDB.

--- database/database.js
+++ database/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.set('useNewUrlParser', true); // Set to true to make all connections set the useNewUrlParser option by default.
 
+mongoose.set('useFindAndModify', false); // Set to true to make all connections set the useNewUrlParser option by default.
+
 mongoose.set('useUnifiedTopology', true); // Set to true to make all connections set the useUnifiedTopology option by default.
 
 mongoose.set('maxTimeMS', 60000); //  Attaches maxTimeMS to every query.
